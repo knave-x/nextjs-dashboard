e832f8cdcf09083d3e0e888d105a41e0f366ac1b
knave-x <59017636+knave-x@users.noreply.github.com>
2024-01-24 13:40:17 +0300

    nextjsLearn

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
-
+import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
@@ -17,3 +18,4 @@ export function Button({ children, className, ...rest }: ButtonProps) {
     </button>
   );
 }
+
